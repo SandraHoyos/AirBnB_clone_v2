dbb49a60a43b3dc94608f4eafa47dcecf6b334f5
SandraHoyos <slhoyosr@gmail.com>
2022-01-13 16:27:23 -0500

    web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env bash
 # Bash script that sets up web servers
 
-
 sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo service nginx start
 ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/web_stati/{releases/test,shared}
+sudo mkdir -p /data/web_static/{releases/test,shared}
 
 echo "
 <html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee -a /data/web_static/releases/test/index.html > /dev/null
+   <head>
+   </head>
+   <body>
+     Holberton School
+   </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-cont="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i 45i $cont" /etc/nginx/sites-available/default
-sudo service nginx reload
+sudo useradd ubuntu
+sudo chown -hR ubuntu:ubuntu /data/
+cont="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+sudo sed -i "45i\ ${cont}" /etc/nginx/sites-available/default
 sudo service nginx start
+sudo service nginx restar
+sudo service nginx reload
