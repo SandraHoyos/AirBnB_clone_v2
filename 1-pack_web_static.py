007dd4a28f29efda9652799ebe9beebe182f2cfe
SandraHoyos <slhoyosr@gmail.com>
2022-01-13 18:37:23 -0500

    pack

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+script generates archive from web_static
+"""
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    function that creates
+    """
+    try:
+        local("mkdir -p versions")
+        date_create = datetime.now().strftime("%Y%m%d%H%M%S")
+        directory = "versions/webstatic{}.tgz".format(date_create)
+        zip_ok = local("tar -cvzf {} web_static".format(directory))
+        return directory
+    except Exception:
+        return None
