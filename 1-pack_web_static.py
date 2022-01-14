01a2e5feff049c59fded7a1facb21c64e2629639
SandraHoyos <slhoyosr@gmail.com>
2022-01-13 19:26:09 -0500

    pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,7 +14,7 @@ def do_pack():
     try:
         local("mkdir -p versions")
         date_create = datetime.now().strftime("%Y%m%d%H%M%S")
-        directory = "versions/webstatic{}.tgz".format(date_create)
+        directory = "versions/webstatic_{}.tgz".format(date_create)
         zip_ok = local("tar -cvzf {} web_static".format(directory))
         return directory
     except Exception:
