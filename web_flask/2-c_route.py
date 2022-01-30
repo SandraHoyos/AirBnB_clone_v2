ba1d305fbc04ac287f4776c7c70160ed84372856
SandraHoyos <slhoyosr@gmail.com>
2022-01-30 16:26:11 -0500

    C is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+script that starts a Flask web application
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """function that returns Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ function that returns HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctext():
+    """ function that returns C followed by the value text variable """
+    parameter = text.replace('_', ' ')
+    return "C {}".format(escape(parameter))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
