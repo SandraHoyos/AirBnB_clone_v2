c89a8bb691348a303cdb52d0785653d5e3bb82e0
SandraHoyos <slhoyosr@gmail.com>
2022-01-30 01:04:59 -0500

    chmod

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,5 +9,10 @@ def hello_world():
     return 'Hello HBNB'
 
 
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
