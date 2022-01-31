3adcf7b6c95a7d5c7e40942a3f10f09025f668bd
SandraHoyos <slhoyosr@gmail.com>
2022-01-30 22:43:38 -0500

    Task 7 flask

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -70,6 +70,7 @@ class FileStorage:
         if obj:
             del self.__objects[obj.__class__.__name__ + '.' + obj.id]
             self.save()
+
     def close(self):
         """
         Calls reload() for deserializing the JSON file to object
